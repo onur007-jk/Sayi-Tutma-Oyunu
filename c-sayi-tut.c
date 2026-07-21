92f9659673c73b686d2b7269fcbb0b7ffaffbcb8
ONUR HARMAN <harmanonur01@gmail.com>
2026-07-21 20:01:04 +0300

    Main içi oluşturuldu

---
+++ c-sayi-tut.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main()
+{
+    int number,hak1=5,hak2=10,hak3=20,hak1sayac=5,hak2sayac=10,hak3sayac=20,tahmin,seviye,puan;
+    int 
+    printf("\n Lütfen oyun seviyesini seciniz: ");
+
+}
